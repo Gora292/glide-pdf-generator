c3c8989e2e79346ea399e735ac34fabc28c08424
Gora292 <156110820+Gora292@users.noreply.github.com>
2025-08-15 19:17:57 +0000

    driver.js

---
+++ driver.js
@@ -0,0 +1,15 @@
+// driver.js
+window.addEventListener("message", async function(event) {
+  const { origin, data: { key, params } } = event;
+  let result;
+  try {
+    result = await window.function(...params);
+  } catch (e) {
+    result = undefined;
+  }
+  const response = { key };
+  if (result !== undefined) {
+    response.result = { type: "string", value: result };
+  }
+  event.source.postMessage(response, "*");
+});
